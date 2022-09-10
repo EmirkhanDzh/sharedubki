fde208628e76783d9114258af0c43bf50c608c3f
Emirkhan Dzhaparov <emdzhaparov@edu.hse.ru>
2022-09-10 19:09:29 +0300

    add image uploading

---
+++ src/main/java/ru/proj/sharedubki/model/Image.java
@@ -0,0 +1,46 @@
+package ru.proj.sharedubki.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "originalFileName")
+    private String originalFileName;
+    @Column(name = "size")
+    private Long size;
+    @Column(name = "contentType")
+    private String contentType;
+    @Column(name = "isPreviewImage")
+    private boolean isPreviewImage;
+    @Column(name = "bytesOfImage")
+    @Lob
+    @Type(type = "org.hibernate.type.ImageType")
+    private byte[] bytesOfImage;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    private Advert advert;
+}
---
+++ src/main/java/ru/proj/sharedubki/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package ru.proj.sharedubki.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.proj.sharedubki.model.Image;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+
+
+}
